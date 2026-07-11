57f065cdb36fbb0e3e216fdf888138ec2cbaa937
VadimShaman <doctor.voronkov@gmail.com>
2026-07-12 01:53:51 +0300

    fix: 10

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,10 @@
 // vite.config.js
-export default {
-    base: '/black_crusade/'
-}
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: '/black_crusade/',
+    // Дополнительно можно указать папку для сборки, если нужно:
+    build: {
+        outDir: 'dist',
+    }
+});
